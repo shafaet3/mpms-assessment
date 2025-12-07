c79ded0c56a0af810c23705f020c5558ab32a98e
shafaet <shafaet3@gmail.com>
2025-12-07 18:38:16 +0600

    Initial project setup

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+     <div className="min-h-screen flex items-center justify-center bg-blue-500">
+      <h1 className="text-white text-4xl font-bold">Hello, Tailwind!</h1>
+    </div>
+  );
+}
